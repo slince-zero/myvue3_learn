a99bf678f9df4a151a0ad015440249295d1b51dd
slince-zero <1711257704@qq.com>
2023-09-14 17:30:32 +0800

    home

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,119 @@
 <template>
-    <div>111</div>
+  <div class="home">
+    <div class="home-card">
+      <el-row :gutter="12">
+        <el-col :span="6">
+          <el-card shadow="hover" style="height: 110px"> Always </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover" style="height: 110px"> Hover </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover" style="height: 110px"> Never </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover" style="height: 110px"> Never </el-card>
+        </el-col>
+      </el-row>
+    </div>
+
+    <div class="home-lineChart">
+      <el-row>
+        <el-col :span="24">
+          <el-card shadow="hover" style="height: 400px">
+            <div ref="chartRef" class="charts" style="height: 400px"></div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+
+    <div class="home-pipeChart">
+      <el-row :gutter="12">
+        <el-col :span="8">
+          <el-card shadow="hover" style="height: 350px"> Always </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="hover" style="height: 350px"> Always </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="hover" style="height: 350px"> Always </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import {
+    Ref,
+    ref,
+    watchEffect,
+    nextTick,
+    onMounted,
+    onBeforeMount,
+    onBeforeUnmount,
+  } from 'vue'
+  import * as echarts from 'echarts'
+  import { EChartsType } from 'echarts'
+
+  const chartRef = ref(null) // 用于获取dom元素
+  const chartDom: Ref<EChartsType | null> = ref(null) // 用于接收mychart内容，因为mychart不是全局对象
+
+  const initCharts = () => {
+    const myChart = echarts.init(chartRef.value)
+    watchEffect(() => {
+      myChart.setOption({
+        xAxis: {
+          data: ['A', 'B', 'C', 'D', 'E'],
+        },
+        yAxis: {},
+        series: [
+          {
+            data: [10, 22, 28, 43, 49],
+            type: 'line',
+            stack: 'x',
+            areaStyle: {},
+          },
+          {
+            data: [5, 4, 3, 5, 10],
+            type: 'line',
+            stack: 'x',
+            areaStyle: {},
+          },
+        ],
+      })
+    })
+    chartDom.value = myChart
+  }
+  //   窗口 resize 事件
+  const handleChartResize = () => {
+    chartDom.value && chartDom.value.resize()
+  }
+
+  const disposeCharts = () => {
+    if (chartDom.value) {
+      chartDom.value.dispose()
+      chartDom.value = null
+    }
+  }
+
+  onBeforeMount(() => {
+    nextTick(() => {
+      initCharts()
+    })
+  })
+
+  onMounted(() => {
+    window.addEventListener('resize', handleChartResize)
+  })
+  onBeforeUnmount(() => {
+    window.removeEventListener('resize', handleChartResize)
+    disposeCharts()
+  })
+</script>
+
+<style scoped lang="scss">
+  .charts {
+    width: 100%;
+  }
+</style>
--- src/views/lend/lend.vue
+++ src/views/lend/lend.vue
@@ -1,1 +1,34 @@
-<template>lend</template>
+<template>
+  <div ref="lendRef" style="height: 400px;">lend</div>
+</template>
+
+<script setup lang="ts">
+    import * as echarts from 'echarts'
+
+  import { ref, onMounted } from 'vue'
+
+  const lendRef = ref()
+
+  onMounted(() => {
+    console.log(lendRef.value) // 打印 DOM 元素的引用
+
+    const myChart = echarts.init(lendRef.value)
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    })
+  })
+</script>
