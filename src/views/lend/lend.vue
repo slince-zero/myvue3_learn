8ee5e9afcfad26b0b2360cdf07cb2509bea3333b
slince-zero <1711257704@qq.com>
2023-09-17 19:48:07 +0800

    lend

--- src/views/lend/lend.vue
+++ src/views/lend/lend.vue
@@ -18,21 +18,59 @@
           <el-button type="info">重置</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">新增</el-button>
+          <el-button type="primary" @click="handleLendPeopleAddOrEdit"
+            >新增</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
 
     <el-table
+      border
       :data="tableData"
       :default-sort="{ prop: 'date', order: 'descending' }"
       style="width: 100%"
     >
-      <el-table-column align="center" prop="id" label="ID" sortable width="180" />
+      <el-table-column
+        align="center"
+        prop="id"
+        label="ID"
+        sortable
+        width="180"
+      />
       <el-table-column align="center" prop="name" label="姓名" width="180" />
-      <el-table-column align="center" prop="money" label="总金额" sortable />
-      <el-table-column align="center" label="操作 " />
+      <el-table-column
+        align="center"
+        prop="money"
+        label="总金额（¥）"
+        sortable
+      />
+      <el-table-column align="center" label="操作 ">
+        <el-button plain :icon="View"></el-button>
+        <el-button plain type="success" :icon="Edit"></el-button>
+        <el-button plain type="danger" :icon="Delete"></el-button>
+      </el-table-column>
     </el-table>
+
+    <el-dialog
+      v-model="dialogLendPeopleFormVisible"
+      width="30%"
+      title="新增"
+      draggable
+    >
+      <el-form :model="lendPeopleForm">
+        <el-form-item label="借款人名称" :width="100">
+          <el-input
+            v-model="lendPeopleForm.borrower"
+            clearable
+            placeholder="请输入借款人名称"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary"> 提交 </el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -73,4 +111,23 @@
       money: 150,
     },
   ]
+
+  // 借款人信息表单
+  const dialogLendPeopleFormVisible = ref(false)
+  const lendPeopleForm = ref({
+    borrower: '',
+  })
+  // 添加或编辑借款人
+  const handleLendPeopleAddOrEdit = () => {
+    dialogLendPeopleFormVisible.value = true
+  }
 </script>
+
+<style scoped lang="scss">
+  .lend {
+    .header-operation {
+      margin-bottom: 3px;
+      text-align: center;
+    }
+  }
+</style>
