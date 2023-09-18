f3038e64407f34db5545449a189d1e556798c177
slince-zero <1711257704@qq.com>
2023-09-18 17:28:14 +0800

    income

--- src/views/income/income.vue
+++ src/views/income/income.vue
@@ -1,17 +1,8 @@
 <template>
-  <el-row :guiter="12">
-    <el-col :span="6">
-      <el-card style="height: 110px">
-        <img class="keda" src="../../assets/home/keda.jpeg" alt="" />
-        <p>11</p>
-      </el-card>
-    </el-col>
-  </el-row>
+  <div class="income">
+    <IncomeCardList />
+  </div>
 </template>
-<style scoped>
-  .keda {
-    width: 100px;
-    height: 100px;
-  }
-  
-</style>
+<script setup lang="ts">
+import IncomeCardList from './income-card-list.vue'
+</script>
